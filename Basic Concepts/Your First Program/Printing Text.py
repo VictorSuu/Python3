791f7e287e9d48e3d8cee79f7a0861172fb043c4
Viktor Su <ouihoo@yahoo.com>
2017-12-29 20:57:26 +0800

    1-1-1

---
+++ Basic Concepts/Your First Program/Printing Text.py
@@ -0,0 +1,10 @@
+//The print statement can also be used to output multiple lines of text.
+//For Example:
+
+print('Hello world!')
+print('Hello world!')
+print('Spam and eggs...')
+
+//Python code often contains referencesto the comedy group Monty Python.This is why
+//the words,"spanm" and "eggs" are soften used as placeholder variables in Python where
+"foo" and "bar" would be used in other programming languages.
