f8971b620ab2d3d025108fa0d255d27b5dfa699d
Viktor Su <ouihoo@yahoo.com>
2017-12-29 21:09:40 +0800

    Create Simple Operations.py

---
+++ Basic Concepts/Simple Operations/Simple Operations.py
@@ -0,0 +1,18 @@
++    plus   
+-    minus
+*    asterisk
+/    forward slash
+()   parentheses
+
+
+Python also carries out multiplication and division, using an asterisk to indicat
+e multiplication and a forward slash to indicate division. 
+
+Use parentheses to determine which operations are performed first.
+>>> 2 * (3 + 4)
+14
+>>> 10 / 2
+5.0
+
+Using a single slash to divide numbers produces a decimal (or float, as it is called in programming). 
+We'll have more about floats in a later lesson.
